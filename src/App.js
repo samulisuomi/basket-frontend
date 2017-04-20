28be7ac39970d72d7db8ac9b6fcaaff40a941507
Samuli Suomi <sasuomi@gmail.com>
2017-04-19 20:49:02 -0700

    Sketch the list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './basket-logo.svg';
 import './App.css';
+
+import { Grid, Row } from 'react-flexbox-grid';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Checkbox from 'material-ui/Checkbox';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { deepPurple800, deepPurple700, deepPurple600 } from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: deepPurple800,
+    primary2Color: deepPurple700,
+    primary3Color: deepPurple600,
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Grid fluid className="App-Grid">
+          <Row className="App-Header Row middle-xs">
+            <img src={logo} className="App-Logo" alt="logo" />
+            <div className="App-Header-Actions">
+              <FlatButton label="New" />
+              <FlatButton label="Share" />
+            </div>
+          </Row>
+          <Row className="App-Container Row middle-xs">
+            <div className="Item-Row">
+              <div className="Item-Checbox" style={{visibility: "hidden"}}>
+                <Checkbox/>
+              </div>
+              <div className="Item-Input">
+                <TextField autoFocus={true} fullWidth={true} placeholder="Add a new item..." />
+              </div>
+              <div className="Item-Trash" style={{visibility: "hidden"}}>
+                <Checkbox/>
+              </div>
+            </div>
+            <div className="Add-Item-Actions">
+              <RaisedButton className="Add-Item-Actions-Button" label="Add"/>
+              <RaisedButton className="Add-Item-Actions-Button" label="Cancel"/>
+            </div>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
