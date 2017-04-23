4a9943d199896de78ab1475f57d40352bda64a39
Samuli Suomi <sasuomi@gmail.com>
2017-04-23 17:59:23 +0300

    Add confirm dialog to New Item

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,6 +9,10 @@ import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
 const mapStateToProps = (state) => {
   return {
     items: state.items
@@ -23,21 +27,66 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const Header = (props) => (
-  <div className="Header">
-    <img src={logo} className="Header-logo" alt="logo" />
-    <IconMenu
-      iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      targetOrigin={{horizontal: 'right', vertical: 'top'}}
-    >
-      <MenuItem primaryText="New list" onTouchTap={props.onNewList}/>
-      <MenuItem primaryText="Invite users" />
-      <MenuItem primaryText="Send via email" />
-      <MenuItem primaryText="Sign out" />
-    </IconMenu>
-  </div>
-);
+class Header extends React.Component {
+  // TODO: a) Each menuitem its own component OR general dialog
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = (confirmed) => {
+    this.setState({open: false});
+    if (confirmed) {
+      this.props.onNewList();
+    }
+  };
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={() => this.handleClose(false)}
+      />,
+      <FlatButton
+        label="Create new"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={() => this.handleClose(true)}
+      />,
+    ];
+
+    return (
+      <div className="Header">
+        <img src={logo} className="Header-logo" alt="logo" />
+        <IconMenu
+          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        >
+          <MenuItem primaryText="New list" onTouchTap={this.handleOpen}/>
+          <MenuItem primaryText="Invite users" />
+          <MenuItem primaryText="Send via email" />
+          <MenuItem primaryText="Sign out" />
+        </IconMenu>
+        <Dialog
+          title="Create new list?"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          All current data will be cleared.
+        </Dialog>
+      </div>
+    );
+  }
+}
 
 export default connect(
   mapStateToProps,
