2f78f620574de92c6d4f61023f3309e42c3f620a
Samuli Suomi <sasuomi@gmail.com>
2017-04-25 05:48:29 +0300

    Change comments in actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,4 @@
-/*
- * action types
- */
+// Action types:
 
 export const ADD_ITEM = 'ADD_ITEM';
 export const EDIT_ITEM = 'EDIT_ITEM';
@@ -13,13 +11,9 @@ export const DELETE_COMMENT = 'REMOVE_COMMENT';
 export const NEW_LIST = 'NEW_LIST';
 export const INVITE_USER = 'INVITE_USER';
 
-/*
- * other constants
- */
+// Other constants:
 
-/*
- * action creators
- */
+// Action creators:
 
 export const addItem = (text) => {
   return {
